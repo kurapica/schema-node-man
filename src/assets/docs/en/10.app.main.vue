bb6a22ae6e7a055e107db97299b3567b02944bfb
kurapica <kurapica125@outlook.com>
2025-09-15 13:37:13 +0800

    update english doc

--- src/assets/docs/en/10.app.main.vue
+++ src/assets/docs/en/10.app.main.vue
@@ -1,6 +1,16 @@
 <template>
     <markdown>
-        ### 10.4 主应用
+        ### 10.4 Main Application
+
+        Data entry and enabling the data push chain present a challenge in establishing a mutual exclusion lock for data updates. Whether using distributed or local locks, a lock key is required. Typically, this is done in the form {application name}_{target ID}.
+
+        However, if data push involves multiple applications, for example, each application aggregates data for a layer of managers, then a safe approach is to identify a sufficiently high-level manager as the target ID to establish a lock.
+
+        To clarify this step, you can specify a master application for the application (this only applies if the application's fields are reference fields). This allows the mutual exclusion lock to be locked at a higher level, avoiding issues.
+
+        ### TODO
+
+        Further discussion of the application's features awaits further discussion after the release of the basic backend SDK.
     </markdown>
 </template>
 
--- src/assets/docs/en/10.app.push.vue
+++ src/assets/docs/en/10.app.push.vue
@@ -1,6 +1,12 @@
 <template>
     <markdown>
-        ### 10.2 字段数据推送
+        ### 10.2 Field Data Push
+
+        The field definition mentions a calculation function. The field using the calculation function is the output field. The calculation function determines the calculation rules, and its parameters indicate the input fields.
+
+        On the front-end interface, only the input fields are typically presented to assist in data entry. After submission, the back-end not only saves the entered data in the database according to the field definition but also checks whether it is an input field for other fields. If so, the corresponding calculation function is called to process it.
+
+        The processed data is then merged into the data in the output field according to the specified data merging rules. Furthermore, an output field can serve as a parameter for other output fields, thus forming a data push chain. Data processing and aggregation are completed during data entry.
     </markdown>
 </template>
 
